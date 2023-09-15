74d0180fe32c830a5293c05814908e597c939c5c
Davina Leong <leong.shi.yun@gmail.com>
2023-09-15 10:52:08 +0800

    Setup app script

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -1,1 +1,15 @@
 console.log(`app.js loaded`)
+
+/// Variables
+
+/// Functions
+function main() {
+  logFunction(`main`)
+}
+
+function logFunction(name: string, params: any = {}): void {
+  console.log(`fn: ${name}:(${JSON.stringify(params)})`)
+}
+
+/// Execute
+document.addEventListener(`DOMContentLoaded`, main)

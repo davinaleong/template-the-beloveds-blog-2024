e43f2d96e601f60a19b1d133f387ebd6c7a96052
Davina Leong <leong.shi.yun@gmail.com>
2023-09-15 10:38:39 +0800

    Imported styles v2

---
+++ src/scripts/config.ts
@@ -0,0 +1,13 @@
+export interface ConfigObjectInterface {
+  appName: string
+  author: string
+  year: number
+  email: string
+}
+
+export const Config: ConfigObjectInterface = {
+  appName: "The Beloved's Blog",
+  author: "Davina Leong",
+  year: 2022,
+  email: "thebelovedsblogtbb@gmail.com",
+}
